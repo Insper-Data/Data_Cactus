85becbf674e004e299586e6b119531f7624ce8ec
Bruno Bregola <brunopbregola@gmail.com>
2021-03-22 22:06:39 -0300

    Update Regressoes.R
    adicionamos algumas regressões com controles

--- Codigo/Resultado/Regressoes.R
+++ Codigo/Resultado/Regressoes.R
@@ -11,6 +11,54 @@ ano_19 <- ano_19 %>%
   mutate(across(36:41, ~ as.numeric(.x)))
 
 
+#########################
+# Juntando os controles #
+#########################
+
+controles1 <- read_xlsx("controleSAEB.xlsx")
+
+controles2 <- read_csv("Controles_2.csv")
+
+
+controles1 <- controles1 %>% select(-1) %>% mutate(ano = 2019)
+
+controles <- left_join(controles2, controles1, by = c("ID_ESCOLA" = "CO_ENTIDADE", "ano"))
+
+
+ano_19 <- ano_19 %>% select(-c("MT_5o",
+                               "IDEB_5o",
+                               "Rede",
+                               "Código da CREDE",
+                               "CREDE",
+                               "Escola",
+                               "Desvio Padrão",
+                              "Indicação do Padrão de Desempenho",
+                               "Muito Crítico",
+                               "Crítico",
+                               "Intermediário",
+                               "Adequado",
+                               "Alunos Previstos",
+                               "Alunos Efetivos",
+                               "Percentual de Participação",
+                               "Taxa de Participação",
+                               "Proficiência Padronizada",
+                              "Fator de Ajuste",
+                               "Número de Alunos no Muito Crítico",
+                               "Número de Alunos no Crítico",
+                               "Número de Alunos no Intermediário",
+                               "Número de Alunos no Adequado",
+                               "IDE EF MT",
+                               "IDE Médio EF",
+                               "Nome do Município",
+                               "Nome da Escola.y"))
+
+ano_19 <- ano_19 %>% na.omit
+
+
+base2019 <- left_join(ano_19, controles, by = c("Código da Escola" = "ID_ESCOLA", "ano"))
+
+
+
 #===============================================
 # Regressões sem controles, dummy cactus
 #===============================================
@@ -26,14 +74,64 @@ ano_19 %>%
   #            linetype = 2)
 
 
-summary(lm(MT_9o ~ cactus, data = ano_19)) # Nota de matemática 9º ano
+#################
+# SEM CONTROLES #
+#################
 
-summary(lm(IDEB_9o ~ cactus, data = ano_19)) # Nota IDEB 9º ano
 
-summary(lm(aprovacao_9o ~ cactus, data = ano_19)) # Taxa de aprovação 9º ano
+summary(lm(MT_9o ~ cactus, data = base2019)) # Nota de matemática 9º ano
 
-summary(lm(abandono_9o ~ cactus, data = ano_19)) # Taxa de abandono 9º ano
+summary(lm(IDEB_9o ~ cactus, data = base2019)) # Nota IDEB 9º ano
 
-summary(lm(reprovacao_9o ~ cactus, data = ano_19)) # Taxa de reprovacao 9º ano
+summary(lm(aprovacao_9o ~ cactus, data = base2019)) # Taxa de aprovação 9º ano
 
-summary(lm(`Proficiência Média` ~ cactus, data = ano_19)) # Proficiência SPAECE 9º ano
+summary(lm(abandono_9o ~ cactus, data = base2019)) # Taxa de abandono 9º ano
+
+summary(lm(reprovacao_9o ~ cactus, data = base2019)) # Taxa de reprovacao 9º ano
+
+summary(lm(`Proficiência Média` ~ cactus, data = base2019)) # Proficiência SPAECE 9º ano
+
+
+#################
+# COM CONTROLES #
+#################
+
+
+summary(lm(MT_9o ~ cactus +
+             porc_cor_prePar +
+             porc_sexo_masc +
+             reprovacao +
+             superior +
+             porc_trans +
+             carro +
+             quanti, data = base2019))
+
+
+summary(lm(IDEB_9o ~ cactus +
+             porc_cor_prePar +
+             porc_sexo_masc +
+             reprovacao +
+             superior +
+             porc_trans +
+             carro +
+             quanti, data = base2019))
+
+
+summary(lm(`Proficiência Média` ~ cactus +
+             porc_cor_prePar +
+             porc_sexo_masc +
+             reprovacao +
+             superior +
+             porc_trans +
+             carro +
+             quanti, data = base2019))
+
+
+summary(lm(reprovacao_9o ~ cactus +
+             porc_cor_prePar +
+             porc_sexo_masc +
+             reprovacao +
+             superior +
+             porc_trans +
+             carro +
+             quanti, data = base2019))
