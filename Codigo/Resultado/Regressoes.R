0adad8d1231abf13205f62a884c19a615ee10c15
Gustavo-luchesi <gustavo_luchesi@outlook.com>
2021-03-23 15:53:47 -0300

    Adicionei controles

--- Codigo/Resultado/Regressoes.R
+++ Codigo/Resultado/Regressoes.R
@@ -2,7 +2,7 @@ library(readxl)
 library(readr)
 library(tidyverse)
 
-base_final <- read_excel("base_final.xlsx")
+base_final <- read_excel("~/Desktop/Data_Cactus/BaseDados/Microdados/base_final.xlsx")
 
 ano_19 <- base_final %>% filter(ano == 2019)
 
@@ -10,20 +10,30 @@ ano_19 <- ano_19 %>%
   mutate(across(36:41, ~ str_replace_all(.x, "NA", NA_character_))) %>% 
   mutate(across(36:41, ~ as.numeric(.x)))
 
+ano_17 <- base_final %>% 
+  filter(ano == 2017) %>% 
+  select(`Código da Escola`, MT_9o, IDEB_9o, `Proficiência Média`) %>% 
+  rename(ID_ESCOLA = `Código da Escola`,
+         MT_9o_17 = MT_9o,
+         IDEB_9o_17 = IDEB_9o,
+         SPAECE_17 = `Proficiência Média`) 
+
+ano_17 <- ano_17 %>% na.omit
 
 #########################
 # Juntando os controles #
 #########################
 
-controles1 <- read_xlsx("controleSAEB.xlsx")
-
-controles2 <- read_csv("Controles_2.csv")
+controles1 <- read_xlsx("~/Desktop/Data_Cactus/BaseDados/Microdados/controleSAEB.xlsx")
+controles2 <- read_csv("~/Desktop/Data_Cactus/BaseDados/Microdados/Controles_2.csv")
+urbano <- read_csv("~/Desktop/Data_Cactus/BaseDados/Microdados/urbano.csv")
 
 
 controles1 <- controles1 %>% select(-1) %>% mutate(ano = 2019)
 
 controles <- left_join(controles2, controles1, by = c("ID_ESCOLA" = "CO_ENTIDADE", "ano"))
 
+urbano <- urbano %>% mutate(urbano17 = ifelse(urbano17 == 2, 0, 1))
 
 ano_19 <- ano_19 %>% select(-c("MT_5o",
                                "IDEB_5o",
@@ -57,6 +67,15 @@ ano_19 <- ano_19 %>% na.omit
 
 base2019 <- left_join(ano_19, controles, by = c("Código da Escola" = "ID_ESCOLA", "ano"))
 
+base2019 <- left_join(base2019, ano_17, by = c("Código da Escola" = "ID_ESCOLA"))
+
+na.omit(base2019) %>% 
+  count(cactus)
+
+base2019 <- na.omit(base2019)
+
+base2019 <- base2019 %>% 
+  left_join(urbano, by = c("Código da Escola" = "ID_ESCOLA"))
 
 
 #===============================================
@@ -104,7 +123,8 @@ summary(lm(MT_9o ~ cactus +
              superior +
              porc_trans +
              carro +
-             quanti, data = base2019))
+             quanti +
+             urbano17, data = base2019))
 
 
 summary(lm(IDEB_9o ~ cactus +
@@ -114,7 +134,8 @@ summary(lm(IDEB_9o ~ cactus +
              superior +
              porc_trans +
              carro +
-             quanti, data = base2019))
+             quanti +
+             urbano17, data = base2019))
 
 
 summary(lm(`Proficiência Média` ~ cactus +
@@ -124,7 +145,8 @@ summary(lm(`Proficiência Média` ~ cactus +
              superior +
              porc_trans +
              carro +
-             quanti, data = base2019))
+             quanti +
+             urbano17, data = base2019))
 
 
 summary(lm(reprovacao_9o ~ cactus +
@@ -135,3 +157,77 @@ summary(lm(reprovacao_9o ~ cactus +
              porc_trans +
              carro +
              quanti, data = base2019))
+
+# Com lag 2017
+
+summary(lm(MT_9o ~ cactus + MT_9o_17, data = base2019))
+summary(lm(IDEB_9o ~ cactus + IDEB_9o_17, data = base2019))
+summary(lm(`Proficiência Média` ~ cactus + SPAECE_17, data = base2019))
+
+summary(lm(MT_9o ~ cactus +
+             porc_cor_prePar +
+             porc_sexo_masc +
+             reprovacao +
+             superior +
+             porc_trans +
+             carro +
+             quanti + 
+             MT_9o_17 +
+             urbano17, data = base2019))
+
+summary(lm(IDEB_9o ~ cactus +
+             porc_cor_prePar +
+             porc_sexo_masc +
+             reprovacao +
+             superior +
+             porc_trans +
+             carro +
+             quanti +
+             IDEB_9o_17 +
+             urbano17, data = base2019))
+
+summary(lm(`Proficiência Média` ~ cactus +
+             porc_cor_prePar +
+             porc_sexo_masc +
+             reprovacao +
+             superior +
+             porc_trans +
+             carro +
+             quanti +
+             SPAECE_17 +
+             urbano17, data = base2019))
+
+# Log-nível
+
+summary(lm(log(MT_9o) ~ cactus +
+             porc_cor_prePar +
+             porc_sexo_masc +
+             reprovacao +
+             superior +
+             porc_trans +
+             carro +
+             quanti + 
+             MT_9o_17 +
+             urbano17, data = base2019))
+
+summary(lm(log(IDEB_9o) ~ cactus +
+             porc_cor_prePar +
+             porc_sexo_masc +
+             reprovacao +
+             superior +
+             porc_trans +
+             carro +
+             quanti +
+             IDEB_9o_17 +
+             urbano17, data = base2019))
+
+summary(lm(log(`Proficiência Média`) ~ cactus +
+             porc_cor_prePar +
+             porc_sexo_masc +
+             reprovacao +
+             superior +
+             porc_trans +
+             carro +
+             quanti +
+             SPAECE_17 +
+             urbano17, data = base2019))
