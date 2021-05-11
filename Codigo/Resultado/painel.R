705a5421665b662eccbbdd467851c630c13773a4
Bruno Bregola <brunopbregola@gmail.com>
2021-05-11 18:15:22 -0300

    Update painel.R

--- Codigo/Resultado/painel.R
+++ Codigo/Resultado/painel.R
@@ -2,17 +2,27 @@ library(tidyverse)
 
 library(idebr)
 
+options(digits = 9)
+
 db <- ideb_fundamental_finais_escolas %>% 
   filter(sigla_uf == "CE",
          rede == "Municipal",
          ano %in% c(2013, 2015, 2017, 2019)) %>% 
-  select(c(4, 5, 7, 14, 17))
+  select(c(4, 5, 7, 14, 17)) %>% 
+  filter(ideb != "") %>% 
+  mutate(id_escola = as.numeric(id_escola),
+         ano = as.numeric(ano),
+         nota_matematica = str_replace_all(nota_matematica, "\\,", "\\."),
+         nota_matematica = str_replace_all(nota_matematica, "(\\d{3}(?!.))", "\\1.00"),
+         nota_matematica = str_replace_all(nota_matematica, "(\\d{3}\\.\\d(?!.))", "\\10"),
+         nota_matematica = as.numeric(nota_matematica),
+         ideb = as.numeric(gsub(",", ".", ideb)))
 
 
-# controles <- read_csv("controles.csv")
+controles <- read_csv("controles.csv")
 
 
-# painel <- left_join(db, controles, by = c("id_escola" = "ID_ESCOLA", "ano" = "ano"))
+painel <- left_join(db, controles, by = c("id_escola" = "ID_ESCOLA", "ano" = "ano"))
 
 
-# write_csv(painel, "painel.csv")
+write_csv(painel, "painel.csv")
