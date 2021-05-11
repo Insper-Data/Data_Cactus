2998622b423c9e6c3a7bd9a87542958579008216
Bruno Bregola <brunopbregola@gmail.com>
2021-05-11 16:22:39 -0300

    Update controles.R

--- Codigo/Resultado/controles/controles.R
+++ Codigo/Resultado/controles/controles.R
@@ -1,13 +1,19 @@
 
 library(tidyverse)
 
-c13 <- read_csv("controles_13.csv")
+c13 <- read_csv("controles_13.csv") 
+
+c13 <- c13 %>% mutate(ano = 2013)
 
 c15 <- read_csv("controles_15.csv")
 
+c15 <- c15 %>% mutate(ano = 2015)
+
 c17 <- read_csv("controles_17.csv")
+
+c17 <- c17 %>% mutate(ano = 2017)
 
 c19 <- read_csv("controles_19.csv")
 
 
-rbind(c13, c15, c17)
+x <- rbind(c13, c15, c17)
